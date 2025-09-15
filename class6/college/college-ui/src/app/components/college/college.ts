d46ba563eb4e334aac57f4597489939f947af0ad
Naveen Sharma <writetonsharma@gmail.com>
2025-09-15 07:29:24 +0530

    c

--- class6/college/college-ui/src/app/components/college/college.ts
+++ class6/college/college-ui/src/app/components/college/college.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { Course } from '../course/course';
+import { Student } from '../student/student';
+import { Enrollment } from '../enrollment/enrollment';
 
 @Component({
   selector: 'ncu-college',
-  imports: [],
+  imports: [Course, Student, Enrollment],
   templateUrl: './college.html',
   styleUrl: './college.css'
 })
 export class College {
 
+    constructor()
+  {
+    // Constructor logic if needed
+  }
+
+  ngOnInit(): void 
+  {
+    // Initialize data or fetch from a service
+
+  }
+  
 }
---
+++ class6/college/college-ui/src/app/components/student/student.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ncu-student',
+  imports: [],
+  templateUrl: './student.html',
+  styleUrl: './student.css'
+})
+export class Student {
+
+}
