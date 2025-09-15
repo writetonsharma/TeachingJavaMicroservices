381d6adfe715d9bdb04bcaa932c7518339cbdbd2
Naveen Sharma <writetonsharma@gmail.com>
2025-09-15 21:40:08 +0530

    c

--- class6/college/college-ui/src/app/app.ts
+++ class6/college/college-ui/src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { College } from './components/college/college'
+import { College } from './components/college/college';
+import { Student } from './components/student/student';
 
 @Component({
   selector: 'ncu-root',
-  imports: [RouterOutlet, College],
+  imports: [RouterOutlet, College, Student],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('CollegeUI');
+  protected readonly title = signal('college-ui');
 }
--- class6/college/college-ui/src/app/components/college/college.ts
+++ class6/college/college-ui/src/app/components/college/college.ts
@@ -1,25 +1,11 @@
 import { Component } from '@angular/core';
-import { Course } from '../course/course';
-import { Student } from '../student/student';
-import { Enrollment } from '../enrollment/enrollment';
 
 @Component({
   selector: 'ncu-college',
-  imports: [Course, Student, Enrollment],
+  imports: [],
   templateUrl: './college.html',
   styleUrl: './college.css'
 })
 export class College {
 
-    constructor()
-  {
-    // Constructor logic if needed
-  }
-
-  ngOnInit(): void 
-  {
-    // Initialize data or fetch from a service
-
-  }
-  
 }
